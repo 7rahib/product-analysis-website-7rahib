51c7ceb376b76f8c0cd519451ca6bd70c7ac3852
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-04-04 22:28:01 +0600

    home page done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const linkClasses = 'mr-2 rounded-lg px-3 py-1 shadow bg-slate-500 text-white hover:bg-white hover:text-black hover:shadow-lg'
+    const linkTailwindClasses = 'mr-2 rounded-lg px-3 py-1 shadow bg-slate-500 text-white hover:bg-white hover:text-black hover:shadow-lg'
     return (
-        <div className='bg-slate-300 p-5 flex justify-center items-center'>
+        <div className='bg-slate-300 p-7 flex justify-center items-center shadow-lg'>
             <ul>
-                <Link className={linkClasses} to="/">Home</Link>
-                <Link className={linkClasses} to="/reviews">Reviews</Link>
-                <Link className={linkClasses} to="/dashboard">Dashboard</Link>
-                <Link className={linkClasses} to="/blogs">Blogs</Link>
+                <Link className={linkTailwindClasses} to="/">Home</Link>
+                <Link className={linkTailwindClasses} to="/reviews">Reviews</Link>
+                <Link className={linkTailwindClasses} to="/dashboard">Dashboard</Link>
+                <Link className={linkTailwindClasses} to="/blogs">Blogs</Link>
             </ul>
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import product from '../../images/product.png'
 
 const Home = () => {
     return (
-        <div>
-            <h1 className='text-3xl font-bold underline'>Home</h1>
-        </div>
+        <div className='flex items-center justify-evenly m-7 p-7'>
+            <div className=''>
+                <h1 className='text-6xl font-bold '>MacBook <span className='underline text-blue-600'>Air</span></h1>
+                <h1 className='text-6xl font-bold text-amber-500'><span>The Future</span></h1>
+                <p className='my-5 w-96'>The MacBook Air is a line of notebook computers developed and manufactured by Apple Inc. It consists of a full-size keyboard, a machined aluminum case, and, in the more modern versions, a thin light structure.</p>
+            </div>
+            <div className='h-2/4 w-2/4'>
+                <img src={product} alt="Laptop" />
+            </div>
+        </div >
     );
 };
 
