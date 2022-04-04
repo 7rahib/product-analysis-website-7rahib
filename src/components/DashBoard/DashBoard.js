44b2fa760db679c44ed7b8f8670b4f8591dd7b65
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-04-04 23:03:29 +0600

    fetch data from json file for chart

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,50 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const DashBoard = () => {
-    const data = [
-        {
-            "month": "Mar",
-            "investment": 100000,
-            "sell": 241,
-            "revenue": 10401
-        },
-        {
-            "month": "Apr",
-            "investment": 200000,
-            "sell": 423,
-            "revenue": 24500
-        },
-        {
-            "month": "May",
-            "investment": 500000,
-            "sell": 726,
-            "revenue": 67010
-        },
-        {
-            "month": "Jun",
-            "investment": 500000,
-            "sell": 529,
-            "revenue": 40405
-        },
-        {
-            "month": "Jul",
-            "investment": 600000,
-            "sell": 601,
-            "revenue": 50900
-        },
-        {
-            "month": "Aug",
-            "investment": 700000,
-            "sell": 670,
-            "revenue": 61000
-        }
-    ]
+
+    const [data, setdata] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setdata(data))
+    }, [])
+
     return (
         <div className='flex justify-center items-center m-12'>
             <div>
                 <div>
-                    <h1 className='text-2xl m-7'>Chart for sell over the month</h1>
+                    <h1 className='text-2xl m-7'>Chart for sell over the months</h1>
                 </div>
                 <div>
                     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
@@ -58,14 +29,13 @@ const DashBoard = () => {
             </div>
             <div>
                 <div>
-                    <h1 className='text-2xl m-7'>Chart for sell over the month</h1>
+                    <h1 className='text-2xl m-7'>Chart for revenue generated over the months</h1>
                 </div>
                 <div>
                     <BarChart width={600} height={300} data={data}>
                         <Bar dataKey="revenue" fill="#8884d8" />
                         <XAxis dataKey="month" />
                         <YAxis />
-                        <Tooltip />
                     </BarChart>
                 </div>
             </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const linkTailwindClasses = 'mr-2 rounded-lg px-3 py-1 shadow bg-slate-500 text-white hover:bg-white hover:text-black hover:shadow-lg'
+    const linkTailwindClasses = 'mr-2 rounded-lg px-5 py-2 shadow bg-slate-500 font-mono text-white hover:bg-white hover:text-black hover:shadow-lg'
     return (
-        <div className='bg-slate-300 p-7 flex justify-center items-center shadow-lg'>
-            <ul>
-                <Link className={linkTailwindClasses} to="/">Home</Link>
-                <Link className={linkTailwindClasses} to="/reviews">Reviews</Link>
-                <Link className={linkTailwindClasses} to="/dashboard">Dashboard</Link>
-                <Link className={linkTailwindClasses} to="/blogs">Blogs</Link>
-            </ul>
+        <div className='bg-slate-300 p-7 flex flex-col justify-center items-center shadow-lg'>
+            <div>
+                <h1 className='text-4xl mb-5 font-bold font-mono '>Product Analysis</h1>
+            </div>
+            <div>
+                <ul>
+                    <Link className={linkTailwindClasses} to="/">Home</Link>
+                    <Link className={linkTailwindClasses} to="/reviews">Reviews</Link>
+                    <Link className={linkTailwindClasses} to="/dashboard">Dashboard</Link>
+                    <Link className={linkTailwindClasses} to="/blogs">Blogs</Link>
+                </ul>
+            </div>
         </div>
     );
 };
